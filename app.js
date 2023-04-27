7a151e3204ee420acb15b1ff4cd2408e8e89c3d8
ronald.f.tavarez <ronald.f.tavarez@gmail.com>
2023-04-27 04:12:51 -0400

    Express test

---
+++ app.js
@@ -0,0 +1,23 @@
+﻿const express = require('express')
+
+const app = express()
+
+app.use((req, res, next) => {
+	console.log("middleware_0")
+	next()
+})
+
+app.use((req, res, next) => {
+	console.log("middleware_1")
+	next()
+})
+
+app.use('/users', (req, res, next) => {
+	res.send("<h1>Welcome to /users<h1>")
+})
+
+app.use('/', (req, res, next) => {
+	res.send("<h1>Welcome to Root!</h1>")
+})
+
+app.listen(3000)
